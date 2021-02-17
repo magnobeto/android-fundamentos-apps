ea6afb6dbff413a29d44f5f8f6b76915e35f6feb
Roberto Magno <roberto.magno@g.globo>
2021-02-17 11:13:12 -0300

    Implementação Login - Parte 2

--- TasksTemplate/app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
+++ TasksTemplate/app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
@@ -1,9 +1,11 @@
 package com.example.tasks.service.repository
 
 import com.example.tasks.service.HeaderModel
+import com.example.tasks.service.constants.TaskConstants
 import com.example.tasks.service.listener.APIListener
 import com.example.tasks.service.repository.remote.PersonService
 import com.example.tasks.service.repository.remote.RetrofitClient
+import com.google.gson.Gson
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -20,6 +22,11 @@ class PersonRepository {
             }
 
             override fun onResponse(call: Call<HeaderModel>, response: Response<HeaderModel>) {
+                if (response.code() != TaskConstants.HTTP.SUCCESS) {
+                    val validation =
+                        Gson().fromJson(response.errorBody().toString(), String::class.java)
+                    listener.onFailure(validation)
+                }
                 response.body()?.let { listener.onSuccess(it) }
             }
 
