50c52cd52dd1c0044ec6cfb30a3d46f82239d63c
Roberto Magno <roberto.magno@g.globo>
2021-02-17 17:43:11 -0300

    Cadastro de Tarefas - Spinner

--- TasksTemplate/app/src/main/java/com/example/tasks/service/repository/PriorityRepository.kt
+++ TasksTemplate/app/src/main/java/com/example/tasks/service/repository/PriorityRepository.kt
@@ -34,4 +34,7 @@ class PriorityRepository(context: Context) {
 
         })
     }
+
+    fun list() = mPriorityDatabase.list()
+
 }
--- TasksTemplate/app/src/main/java/com/example/tasks/viewmodel/TaskFormViewModel.kt
+++ TasksTemplate/app/src/main/java/com/example/tasks/viewmodel/TaskFormViewModel.kt
@@ -2,7 +2,20 @@ package com.example.tasks.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.tasks.service.model.PriorityModel
+import com.example.tasks.service.repository.PriorityRepository
 
 class TaskFormViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val mPriorityRepository = PriorityRepository(application)
+
+    private val mPriorityList = MutableLiveData<List<PriorityModel>>()
+    var priorities: LiveData<List<PriorityModel>> = mPriorityList
+
+    fun listPriorities() {
+        mPriorityList.value = mPriorityRepository.list()
+    }
 
 }
