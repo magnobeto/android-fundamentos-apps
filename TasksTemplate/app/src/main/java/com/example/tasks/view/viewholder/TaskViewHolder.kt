92ecde3ccd2cb6ae40164194a8ac1508929c1224
Roberto Magno <roberto.magno@g.globo>
2021-02-21 20:23:54 -0300

    Listagem de Tarefas - ViewHolder

--- TasksTemplate/app/src/main/java/com/example/tasks/view/viewholder/TaskViewHolder.kt
+++ TasksTemplate/app/src/main/java/com/example/tasks/view/viewholder/TaskViewHolder.kt
@@ -1,6 +1,7 @@
 package com.example.tasks.view.viewholder
 
 import android.app.AlertDialog
+import android.graphics.Color
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
@@ -8,9 +9,15 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.tasks.R
 import com.example.tasks.service.listener.TaskListener
 import com.example.tasks.service.model.TaskModel
+import com.example.tasks.service.repository.PriorityRepository
+import java.text.SimpleDateFormat
+import java.util.*
 
 class TaskViewHolder(itemView: View, val listener: TaskListener) :
     RecyclerView.ViewHolder(itemView) {
+
+    private val mDateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+    private val mPriorityRepository = PriorityRepository(itemView.context)
 
     private var mTextDescription: TextView = itemView.findViewById(R.id.text_description)
     private var mTextPriority: TextView = itemView.findViewById(R.id.text_priority)
@@ -20,15 +27,31 @@ class TaskViewHolder(itemView: View, val listener: TaskListener) :
     /**
      * Atribui valores aos elementos de interface e também eventos
      */
-    fun bindData(taskModel: TaskModel) {
+    fun bindData(task: TaskModel) {
 
-        this.mTextDescription.text = ""
-        this.mTextPriority.text = ""
-        this.mTextDueDate.text = ""
+        this.mTextDescription.text = task.description
+        this.mTextPriority.text = mPriorityRepository.getDescription(task.priorityId)
+
+        val date = SimpleDateFormat("yyyy-MM-dd").parse(task.dueDate)
+        this.mTextDueDate.text = mDateFormat.format(date)
+
+        if (task.complete) {
+            mTextDescription.setTextColor(Color.GRAY)
+            mImageTask.setImageResource(R.drawable.ic_done)
+        } else {
+            mTextDescription.setTextColor(Color.BLACK)
+            mImageTask.setImageResource(R.drawable.ic_todo)
+        }
 
         // Eventos
-        // mTextDescription.setOnClickListener { listener.onListClick(task.id) }
-        // mImageTask.setOnClickListener { }
+        mTextDescription.setOnClickListener { listener.onListClick(task.id) }
+        mImageTask.setOnClickListener { view ->
+            if (task.complete) {
+                listener.onUndoClick(task.id)
+            } else {
+                listener.onCompleteClick(task.id)
+            }
+        }
 
         mTextDescription.setOnLongClickListener {
             AlertDialog.Builder(itemView.context)
