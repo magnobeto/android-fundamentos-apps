cc55a3370a87172dcc30092ee5befe943929311d
Roberto Magno <roberto.magno@g.globo>
2021-07-20 00:06:33 -0300

    TimePicker e Eventos

--- Componentes/app/src/main/java/com/example/componentes/TimeActivity.kt
+++ Componentes/app/src/main/java/com/example/componentes/TimeActivity.kt
@@ -1,10 +1,12 @@
 package com.example.componentes
 
 import android.app.DatePickerDialog
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.DatePicker
+import android.widget.TimePicker
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.text.SimpleDateFormat
@@ -13,7 +15,8 @@ import java.util.*
 class TimeActivity :
     AppCompatActivity(),
     View.OnClickListener,
-    DatePickerDialog.OnDateSetListener {
+    DatePickerDialog.OnDateSetListener,
+    TimePicker.OnTimeChangedListener {
 
     private val mSimpleDate = SimpleDateFormat("dd/MM/yyyy")
 
@@ -35,6 +38,32 @@ class TimeActivity :
 
                 DatePickerDialog(this, this, year, month, day).show()
             }
+            R.id.button_get_time -> {
+                val timePicker = findViewById<TimePicker>(R.id.timepicker)
+
+                if (Build.VERSION.SDK_INT >= 23) {
+                    val hour = timePicker.hour
+                    val minute = timePicker.minute
+
+                    toast("$hour:$minute")
+                } else {
+                    val hour = timePicker.currentHour
+                    val minute = timePicker.currentMinute
+
+                    toast("$hour:$minute")
+                }
+            }
+            R.id.button_set_time -> {
+                val timePicker = findViewById<TimePicker>(R.id.timepicker)
+
+                if (Build.VERSION.SDK_INT >= 23) {
+                    timePicker.hour = 20
+                    timePicker.minute = 20
+                } else {
+                    timePicker.currentHour = 20
+                    timePicker.currentMinute = 20
+                }
+            }
         }
     }
 
@@ -46,11 +75,18 @@ class TimeActivity :
         buttonDate.text = mSimpleDate.format(date.time)
     }
 
+    override fun onTimeChanged(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        toast("$hourOfDay:$minute")
+    }
+
     private fun toast(str: String) {
         Toast.makeText(this, str, Toast.LENGTH_SHORT).show()
     }
 
     private fun setListeners() {
         findViewById<Button>(R.id.button_date).setOnClickListener(this)
+        findViewById<TimePicker>(R.id.timepicker).setOnTimeChangedListener(this)
+        findViewById<Button>(R.id.button_get_time).setOnClickListener(this)
+        findViewById<Button>(R.id.button_set_time).setOnClickListener(this)
     }
 }
